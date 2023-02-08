6e3be8689a801a560431f0d15acc9d23c70e6dbe
Wenbin Li <xunyuanxin@163.com>
2023-02-08 14:16:18 -0500

    Update accelerometer_landing.sql

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,13 +1,9 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`customer_landing` (
-  `customerName` string,
-  `email` string,
-  `phone` string,
-  `birthDay` string,
-  `serialNumber` string,
-  `registrationDate` bigint,
-  `lastUpdateDate` bigint,
-  `shareWithResearchAsOfDate` bigint,
-  `shareWithPublicAsOfDate` bigint
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing` (
+  `user` string,
+  `timeStamp` bigint,
+  `x` float,
+  `y` float,
+  `z` float
 )
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 WITH SERDEPROPERTIES (
@@ -17,5 +13,5 @@ WITH SERDEPROPERTIES (
   'mapping' = 'TRUE'
 )
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://chaolizi-stedi-lakehouse/customer/landing/'
+LOCATION 's3://chaolizi-stedi-lakehouse/accelerometer/landing/'
 TBLPROPERTIES ('classification' = 'json');
